eb302547959f595e2ca8675fdaf83d1a60b45a97
profharris <profharris@users.noreply.github.com>
2025-06-21 19:57:22 -0500

    Create MAX98357-I2S-stream-from-PROGMEM.ino

---
+++ code/MAX98357-I2S-stream-from-PROGMEM/MAX98357-I2S-stream-from-PROGMEM.ino
@@ -0,0 +1,216 @@
+/* MAX98357-I2S-stream-from-PROGMEM.ino
+ * Requires a MAX98357A I²S Audio Amplifier/Speaker
+ * ESP32-S3 1.54in TFT Expansion Board with Speaker
+ *
+ *      Stream/Play an Audio file to the I²S bus from PROGMEM:
+ *      https://forum.arduino.cc/t/arduino-and-esp32-i2s/501975/4
+ *
+ * Requires a music file in the same directory:  “soundsample.h”
+ *
+ * Wiring:
+ * ¯¯¯¯¯¯¯
+ * MAX98357A I²S Amp (7-pins)       ESP32-S3-WROOM-1
+ * 1. VIN  (Power)              --> 3V3
+ * 2. GND                       --> GND
+ * 3. SD   (L/R Channel Select) --> Connect to 3V3 (0Ω = Left)
+ * 4. GAIN                      --> Connect to GND (12dB Gain)
+ * 5. DIN  (Serial Data In/Out) --> GPIO7
+ * 6. BCLK (Bit Clock)          --> GPIO15
+ * 7. LRC  (Left Right Clock)   --> GPIO16
+ *
+ * External Speaker interface: (⧾ ⧿)
+ * Audio⧾  Connect to Speaker Positive (usually Red wire)
+ * Audio⧿  Connect to Speaker Negative
+ *
+ * REF: // https://busyducks.com/comprehensive-example-code-demonstrating-using-arduino-flash-memory-via-progmem/
+ */
+
+/*******************************************************************
+Type-C USB Data cable plugged into Left-side ESP32-S3 USB-OTG port.
+                                   ¯¯¯¯¯¯¯¯¯          ¯¯¯¯¯¯¯
+Arduino IDE > Tools                                  [CONFIGURATION]
+                   Board: "ESP32S3 Dev Module"
+         USB CDC On Boot: "Enabled"  **Cable plugged into Left USB**
+           CPU Frequency: "240MHz (WiFi)"                 ¯¯¯¯
+         USB DFU On Boot: "Disabled"
+              Flash Mode: "QIO 80MHz"
+              Flash Size: "16MB 128Mb"
+USB Firmware MSC On Boot: "Disabled"
+        Partition Scheme: "16M Flash (3MB APP/9.9MB FATFS)"
+                   PSRAM: "OPI PSRAM"
+             Upload Mode: "UART0/Hardware CDC"
+            Upload Speed: "115200"
+                USB Mode: "Hardware CDC and JTAG"
+*******************************************************************/
+ 
+#include <pgmspace.h>           // Arduino PROGMEM library
+#include "driver/i2s.h"         // Include newer ESP-IDF I²S library
+
+#include "soundsample.h"        // 16-bit 16kHz wav audio sample (static)
+
+int i2s_num = 0;                // I²S port number
+i2s_config_t i2s_config = {     // I²S configuration
+  .mode                 = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+  .sample_rate          = 16000, // 16-bit, 16kHz wav sample
+  .bits_per_sample      = I2S_BITS_PER_SAMPLE_16BIT,
+  .channel_format       = I2S_CHANNEL_FMT_RIGHT_LEFT,
+  .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+  .intr_alloc_flags     = ESP_INTR_FLAG_LEVEL1, // High IRQ priority
+  .dma_buf_count        = 8,
+  .dma_buf_len          = 64,
+  .use_apll             = true   // use internal clock, better audio
+};
+
+i2s_pin_config_t pin_config = {
+  .bck_io_num           = 15, // MAX98357A I²S BCLK pin
+  .ws_io_num            = 16, // MAX98357A I²S LRC  pin
+  .data_out_num         =  7, // MAX98357A I²S DOUT pin
+  .data_in_num          = I2S_PIN_NO_CHANGE // Not used
+};
+
+
+// Main function to Play samples from PROGMEM
+void playPROGMEMsample(const uint32_t *audioSample) {
+
+  uint32_t sampleSize = sizeof(audioSample) * 4;
+  uint32_t counter    = 0;
+
+  // Initialize I²S with configurations above
+  i2s_driver_install((i2s_port_t)i2s_num, &i2s_config, 0, NULL);
+  i2s_set_pin((i2s_port_t)i2s_num, &pin_config);
+  // Set sample rate of I²S to sample rate of audio file
+  i2s_set_sample_rates((i2s_port_t)i2s_num, 16000);
+
+  uint32_t readData;
+  while(audioSample)  {
+    readData = pgm_read_dword(&audioSample[counter++]);
+    if(!readData) break; // NULL
+    i2s_write_sample_nb(readData);         // Send sample to I²S bus
+  }
+  // Stop & uninstall I²S driver
+  i2s_driver_uninstall((i2s_port_t)i2s_num);
+}
+
+size_t i2s_bytes_written = 0;
+bool i2s_write_sample_nb(uint32_t sample) { // Send sample to I²S bus
+  i2s_write((i2s_port_t)i2s_num, (const uint32_t *)&sample,
+            sizeof(uint32_t), (size_t *)&i2s_bytes_written, 100);
+  if(!i2s_bytes_written) 
+      return false;
+  else
+      return true;
+}
+
+
+void setup() {
+  Serial.begin(115200);             // Initialize the Serial Monitor
+  while(!Serial);                   // Wait for Serial Port to open
+
+  Serial.println("Listen to the music sample in PROGMEM!");
+  playPROGMEMsample(sample1);
+}
+
+void loop() {
+    // Listen to the music...
+}
+
+/*******************************************************************
+Warning:
+¯¯¯¯¯¯¯¯
+ 1. Warning: #warning "This set of I2S APIs has been deprecated,
+    please include 'driver/i2s_std.h', 'driver/i2s_pdm.h' or 'driver/i2s_tdm.h'
+    instead. if you want to keep using the old APIs and ignore this warning,
+    you can enable 'Suppress legacy driver deprecated warning' option under
+    'I2S Configuration' menu in Kconfig" [-Wcpp]
+
+    27 | #warning "This set of I2S APIs has been deprecated, \
+       |  ^~~~~~~
+
+Sketch uses 735826 bytes (23%) of program storage space. 
+Maximum is 3145728 bytes.
+Global variables use 21252 bytes (6%) of dynamic memory, 
+            leaving 306428 bytes for local variables. 
+         Maximum is 327680 bytes.
+esptool.py v4.8.1
+Serial port COM8
+Connecting...
+
+Chip is ESP32-S3 (QFN56) (revision v0.2)
+Features: WiFi, BLE, Embedded PSRAM 8MB (AP_3v3)
+Crystal is 40MHz
+MAC: 30:ed:a0:bb:73:9c
+
+Uploading stub...
+Running stub...
+Stub running...
+
+Configuring flash size...
+Flash will be erased from 0x00000000 to 0x00004fff...
+Flash will be erased from 0x00008000 to 0x00008fff...
+Flash will be erased from 0x0000e000 to 0x0000ffff...
+Flash will be erased from 0x00010000 to 0x000c3fff...
+Compressed 20208 bytes to 13058...
+Writing at 0x00000000... (100 %)
+Wrote 20208 bytes (13058 compressed) at 0x00000000 in 0.4 seconds 
+  (effective 427.5 kbit/s)...
+Hash of data verified.
+Compressed 3072 bytes to 144...
+Writing at 0x00008000... (100 %)
+Wrote 3072 bytes (144 compressed) at 0x00008000 in 0.1 seconds 
+  (effective 434.9 kbit/s)...
+Hash of data verified.
+Compressed 8192 bytes to 47...
+Writing at 0x0000e000... (100 %)
+Wrote 8192 bytes (47 compressed) at 0x0000e000 in 0.1 seconds 
+  (effective 691.0 kbit/s)...
+Hash of data verified.
+Compressed 735968 bytes to 569055...
+Writing at 0x00010000... (2 %)
+Writing at 0x0001af9e... (5 %)
+Writing at 0x0001ef00... (8 %)
+Writing at 0x00023049... (11 %)
+Writing at 0x000271ea... (14 %)
+Writing at 0x0002b3b6... (17 %)
+Writing at 0x0002f59c... (20 %)
+Writing at 0x00033886... (22 %)
+Writing at 0x00037a55... (25 %)
+Writing at 0x0003bb64... (28 %)
+Writing at 0x0003fce6... (31 %)
+Writing at 0x00043f1a... (34 %)
+Writing at 0x00048108... (37 %)
+Writing at 0x0004c31c... (40 %)
+Writing at 0x0005083a... (42 %)
+Writing at 0x00054925... (45 %)
+Writing at 0x000589e1... (48 %)
+Writing at 0x0005ca9a... (51 %)
+Writing at 0x00060c6f... (54 %)
+Writing at 0x00064e4c... (57 %)
+Writing at 0x00069241... (60 %)
+Writing at 0x0006d659... (62 %)
+Writing at 0x00071783... (65 %)
+Writing at 0x000758e7... (68 %)
+Writing at 0x000799f0... (71 %)
+Writing at 0x000865ac... (74 %)
+Writing at 0x0008c86b... (77 %)
+Writing at 0x0009242a... (80 %)
+Writing at 0x00097d61... (82 %)
+Writing at 0x0009d43c... (85 %)
+Writing at 0x000a2eb4... (88 %)
+Writing at 0x000a8a3b... (91 %)
+Writing at 0x000af760... (94 %)
+Writing at 0x000b93a7... (97 %)
+Writing at 0x000bf158... (100 %)
+Wrote 735968 bytes (569055 compressed) at 0x00010000 in 6.8 seconds 
+  (effective 861.0 kbit/s)...
+Hash of data verified.
+
+Leaving...
+Hard resetting with RTC WDT...
+
+--------------------------------------------------------------------
+Serial Monitor:
+¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+19:39:19.870 -> ESP-ROM:esp32s3-20210327
+
+19:39:19.988 -> Listen to the music sample in PROGMEM!
+*******************************************************************/
